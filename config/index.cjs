fa7efeefb7074875f7ea0344384c68aa99917115
lth66 <51687687+lth66@users.noreply.github.com>
2023-05-14 23:12:22 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oHmdC50qcQdu8bUfSz3x8uT_RGbY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'lKYTHb2LRO_-x2AgjnnxhUFLNg0x5OQHDj8oAAJRiNE',
+      useTemplateId: 'KPrQdxJgqPrZ-tRF9zbu7dqW32y30jFCZPPK9OyMlO8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-31',
       festivals: [
