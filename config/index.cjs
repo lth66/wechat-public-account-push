71107f2418f1e9c3842641811cb018adc1e3a64f
lth66 <51687687+lth66@users.noreply.github.com>
2023-05-14 23:08:32 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -15,36 +15,15 @@ const USER_CONFIG = {
 
   PROVINCE: '上海',
   CITY: '上海',
-  
-  TIAN_API: {
-    // 天行API KEY，如果使用天行API则需要填写此项
-    key: '4f1278689de11841b6d45c7818b59608',
-    
-    /** 天行API相关，需要config中配置 TIAN_API_KEY  */
-    // 早安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    morningGreeting: true,
-  
-    // 晚安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
-    eveningGreeting: false,
-  
-    // 天行天气（展示未来N天，最多7天）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
-    weather: 0,
-  
-    // 全网热搜榜（展示N条，最多30条）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
-    networkHot: 3,
-  
-    // 全网热搜榜展示类型，默认展示概要信息: ['title': 仅展示标题, 'default': 展示概要信息]
-    networkHotType: 'default',
-  },
-  
+
   USERS: [
     {
       // 想要发送的人的名字
       name: '佳宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oHmdC5yepzJDqqTdm8YQGPzfDMUA',
+      id: 'oHmdC50qcQdu8bUfSz3x8uT_RGbY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0scqP2KeDxV_E94won5ECafGiCk0voyHoy4crzNbccY',
+      useTemplateId: 'lKYTHb2LRO_-x2AgjnnxhUFLNg0x5OQHDj8oAAJRiNE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-31',
       festivals: [
@@ -67,46 +46,6 @@ const USER_CONFIG = {
         },
         {
           type: '*生日', name: '爸', year: '1969', date: '09-02',
-        },
-      ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2015-11-22' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2021-05-02' },
-      ],
-    },
-	
-	{
-      // 想要发送的人的名字
-      name: '我',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oHmdC50qcQdu8bUfSz3x8uT_RGbY',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0scqP2KeDxV_E94won5ECafGiCk0voyHoy4crzNbccY',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '05-31',
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '佳宝贝', year: '1996', date: '04-15',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '*生日', name: '我', year: '1996', date: '06-21',
-        },
-        {
-          type: '*生日', name: '妈', year: '1967', date: '12-14',
-        },
-        {
-          type: '*生日', name: '爸', year: '1967', date: '10-24',
-        },
-        {
-          type: '*生日', name: '佳妈', year: '1973', date: '02-22',
-        },
-        {
-          type: '*生日', name: '佳爸', year: '1969', date: '09-02',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
