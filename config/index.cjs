88ce607c4036e5afa059540f21ae5c39605f252b
lth66 <51687687+lth66@users.noreply.github.com>
2023-05-14 23:39:09 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -44,7 +44,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oHmdC5yepzJDqqTdm8YQGPzfDMUA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'lKYTHb2LRO_-x2AgjnnxhUFLNg0x5OQHDj8oAAJRiNE',
+      useTemplateId: 'vjRA-LXoiosebmoSib100kz0kguNgH6aSGS_tKDsP1E',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-31',
       festivals: [
@@ -89,7 +89,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oHmdC50qcQdu8bUfSz3x8uT_RGbY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'lKYTHb2LRO_-x2AgjnnxhUFLNg0x5OQHDj8oAAJRiNE',
+      useTemplateId: 'vjRA-LXoiosebmoSib100kz0kguNgH6aSGS_tKDsP1E',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-31',
       festivals: [
